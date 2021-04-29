2e439c6a66aded8e9bfb4b5709e14fc1d0213444
gawronCode <gawroncode@gmail.com>
2021-04-30 01:00:45 +0200

    Add GitHubController for github api use

--- ITCenter_dokumenty_magazynowe/Models/ViewModels/WarehouseDocVM.cs
+++ ITCenter_dokumenty_magazynowe/Models/ViewModels/WarehouseDocVM.cs
@@ -5,7 +5,13 @@ using System.Threading.Tasks;
 
 namespace ITCenter_dokumenty_magazynowe.Models.NewFolder
 {
-    public class WarehouseDocVM
+    public class WarehouseDocVm
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ClientNumber { get; set; }
+        public DateTime? Date { get; set; }
+        public int NetPrice { get; set; }
+        public int GrossPrice { get; set; }
     }
 }

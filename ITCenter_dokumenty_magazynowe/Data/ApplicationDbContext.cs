a29da94fc468c5133a5a48bb4fbae4344beaa421
gawronCode <gawroncode@gmail.com>
2021-04-29 21:51:40 +0200

    Add project files.

---
+++ ITCenter_dokumenty_magazynowe/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITCenter_dokumenty_magazynowe.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ ITCenter_dokumenty_magazynowe/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ITCenter_dokumenty_magazynowe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

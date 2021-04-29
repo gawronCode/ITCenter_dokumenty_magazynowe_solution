e71dcafea7b81f72cfbc7d6feb0c2cc029542ad8
gawronCode <gawroncode@gmail.com>
2021-04-29 23:31:43 +0200

    Add Employee model (for registration verification), add ViewModels folder and WarehouseDocVM, modified Layout and Index page

---
+++ ITCenter_dokumenty_magazynowe/Models/DbModels/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITCenter_dokumenty_magazynowe.Models.DbModels
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ ITCenter_dokumenty_magazynowe/Models/ViewModels/WarehouseDocVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITCenter_dokumenty_magazynowe.Models.NewFolder
+{
+    public class WarehouseDocVM
+    {
+    }
+}

5fc94a14a3edb3e1e1154376fd8a227dc95f853f
gawronCode <gawroncode@gmail.com>
2021-04-29 23:33:20 +0200

    Add annotations to Employee DbModel

--- ITCenter_dokumenty_magazynowe/Models/DbModels/Employee.cs
+++ ITCenter_dokumenty_magazynowe/Models/DbModels/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace ITCenter_dokumenty_magazynowe.Models.DbModels
 {
     public class Employee
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Email { get; set; }
     }
 }

3c26f7fb4655f470a2059a074a99e4dd8099e4b3
gawronCode <gawroncode@gmail.com>
2021-05-01 12:50:05 +0200

    Implemented gitHub api scraper

--- ITCenter_dokumenty_magazynowe/Controllers/GitHubController.cs
+++ ITCenter_dokumenty_magazynowe/Controllers/GitHubController.cs
@@ -1,13 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using ITCenter_dokumenty_magazynowe.Models.ApiModel;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 
 namespace ITCenter_dokumenty_magazynowe.Controllers
 {
     public class GitHubController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            
+
+            var httpClient = new HttpClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                "https://api.github.com/repos/gawronCode/ITCenter_dokumenty_magazynowe_solution");
+            var headerValue = new ProductInfoHeaderValue("WarehouseApp", "1.0");
+            request.Headers.UserAgent.Add(headerValue);
+            var response = await httpClient.SendAsync(request);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var json = JObject.Parse(responseBody);
+
+            var model = new GitHubRepoData
+            {
+                CreationDate = json["created_at"].ToString(),
+                Description = json["description"].ToString(),
+                Id = json["id"].ToString(),
+                Name = json["name"].ToString(),
+                OwnerName = json["owner"]["login"].ToString(),
+                SubscribersCount = json["subscribers_count"].ToString(),
+            };
+
+            return View(model);
         }
     }
 }
---
+++ ITCenter_dokumenty_magazynowe/Models/ApiModel/GitHubRepoData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITCenter_dokumenty_magazynowe.Models.ApiModel
+{
+    public class GitHubRepoData
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CreationDate { get; set; }
+        public string SubscribersCount { get; set; }
+        public string OwnerName { get; set; }
+
+
+    }
+}
